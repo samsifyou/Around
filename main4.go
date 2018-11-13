1a3f49ac094d34ac4eaec78b75ef6fcc08a4e211
sifang <samsifyou@gmail.com>
2018-11-13 10:33:55 -0800

    main4.go 2018-11-13

---
+++ main4.go
@@ -0,0 +1,262 @@
+package main
+
+import (
+	"context"
+    "cloud.google.com/go/storage"
+	elastic "gopkg.in/olivere/elastic.v3"
+	"fmt"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"reflect"
+	"github.com/pborman/uuid"
+)
+
+const (
+	INDEX = "around"
+	TYPE = "post"
+	DISTANCE = "200km"
+	// Needs to update
+    //PROJECT_ID = "around-xxx"
+	//BT_INSTANCE = "around-post"
+	// Needs to update this URL if you deploy it to cloud.
+	ES_URL = "http://35.224.88.57:9200/"
+	//ES_URL = "http://35.237.131.231:9200"
+
+	BUCKET_NAME = "post-images-220808"
+
+)
+
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
+
+type Post struct {
+	User string `json:"user"`
+	Message string `json:"message"`
+	Location Location `json:"location"`
+	Url    string `json:"url"`
+}
+
+
+func main() {
+	// Create a client
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// Use the IndexExists service to check if a specified index exists.
+	exists, err := client.IndexExists(INDEX).Do()
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		// Create a new index.
+		mapping := `{
+			"mappings":{
+				"post":{
+					"properties":{
+						"location":{
+							"type":"geo_point"
+						}
+					}
+				}
+			}
+		}`
+		_, err := client.CreateIndex(INDEX).Body(mapping).Do()
+		if err != nil {
+			// Handle error
+			panic(err)
+		}
+	}
+
+
+	fmt.Println("started-service")
+	http.HandleFunc("/post", handlerPost)
+	http.HandleFunc("/search", handlerSearch)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+
+	 w.Header().Set("Content-Type", "application/json")
+     w.Header().Set("Access-Control-Allow-Origin", "*")
+     w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+
+      // 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
+      // After you call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
+      // If the file size is larger than maxMemory, the rest of the data will be saved in a system temporary file.
+      r.ParseMultipartForm(32 << 20)
+
+      // Parse from form data.
+      fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+      lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+      lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+      p := &Post{
+             User:    "1111",
+             Message: r.FormValue("message"),
+             Location: Location{
+                    Lat: lat,
+                    Lon: lon,
+             },
+      }
+
+      id := uuid.New()
+
+      file, _, err := r.FormFile("image")
+      if err != nil {
+             http.Error(w, "Image is not available", http.StatusInternalServerError)
+             fmt.Printf("Image is not available %v.\n", err)
+             return
+      }
+      defer file.Close()
+
+      ctx := context.Background()
+
+     // replace it with your real bucket name.
+      _, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+      if err != nil {
+             http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+             fmt.Printf("GCS is not setup %v\n", err)
+             return
+      }
+
+      // Update the media link after saving to GCS.
+      p.Url = attrs.MediaLink
+
+      // Save to ES.
+      saveToES(p, id)
+
+      // Save to BigTable.
+      //saveToBigTable(p, id)
+}
+
+func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+      client, err := storage.NewClient(ctx)
+      if err != nil {
+             return nil, nil, err
+      }
+      defer client.Close()
+
+      bucket := client.Bucket(bucketName)
+      // Next check if the bucket exists
+      if _, err = bucket.Attrs(ctx); err != nil {
+             return nil, nil, err
+      }
+
+      obj := bucket.Object(name)
+      w := obj.NewWriter(ctx)
+      if _, err := io.Copy(w, r); err != nil {
+             return nil, nil, err
+      }
+      if err := w.Close(); err != nil {
+             return nil, nil, err
+      }
+
+      
+      if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+             return nil, nil, err
+      }
+
+      attrs, err := obj.Attrs(ctx)
+      fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
+      return obj, attrs, err
+}
+
+func saveToES(p *Post, id string) {
+	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL),
+		elastic.SetSniff(false))
+
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = es_client.Index().
+		Index(INDEX).
+		Type(TYPE).
+		Id(id).
+		BodyJson(p).
+		Refresh(true).
+		Do()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Post is saved to index: %s\n", p.Message)
+}
+
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search:\n")
+	  /*
+      lat := r.URL.Query().Get("lat")
+      lon := r.URL.Query().Get("lon")
+	  
+	  fmt.Printf("Search received: %s %s", lat, lon)
+	  fmt.Fprintf(w, "Search received: %s %s", lat, lon)*/
+	  
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+    lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+    // range is optional 
+    ran := DISTANCE 
+    if val := r.URL.Query().Get("range"); val != "" { 
+        ran = val + "km" 
+    }
+
+	fmt.Printf("Search received: %f %f %s \n", lat, lon, ran)
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	  
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	q := elastic.NewGeoDistanceQuery("location")
+	q = q.Distance(ran).Lat(lat).Lon(lon)
+	fmt.Printf("q: T: %T    v: %v     d: %d    s: %s  \n", q, q, q, q)
+	searchResult, err := client.Search().
+		Index(INDEX). // "around"
+		Query(q).
+		Pretty(true).
+		Do()
+
+	fmt.Printf("searchResult: T: %T    v: %v     d: %d    s: %s \n", searchResult, searchResult, searchResult, searchResult)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Query took %d milliseconds\n", searchResult.TookInMillis)
+	fmt.Printf("Found a total of %d posts\n", searchResult.TotalHits())
+
+	var typ Post
+	var ps []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) { // java: instance of
+		fmt.Printf("item: T: %T    v: %v     d: %d    s: %s \n", item, item, item, item)
+		p := item.(Post) // type conversion in java: p = (Post) item
+		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", 
+			p.User, p.Message, p.Location.Lat, p.Location.Lon)
+		ps = append(ps, p)
+	}
+
+	js, err := json.Marshal(ps)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(js)
+
+}
+
+
+
